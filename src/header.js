f8b3c140534b36c89a99834c7c5d700d499b3e78
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-09-28 16:08:12 +0800

    Create functions to clear and populate content from header buttons

--- src/header.js
+++ src/header.js
@@ -1,3 +1,13 @@
+import createHome from "./home";
+import createMenu from "./menu";
+
+const clearContent = () => {
+  const content = document.querySelector('#content');
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+}
+
 const createHeader = () => {
   const header = document.createElement('header');
 
@@ -7,16 +17,31 @@ const createHeader = () => {
 
   const list = document.createElement('ul');
   const home = document.createElement('li');
-  home.id = 'home';
+  home.className = 'headerBtn';
   home.textContent = 'Home';
 
   const menu = document.createElement('li');
-  menu.id = 'menu';
+  menu.className = 'headerBtn';
   menu.textContent = 'Menu';
 
   const contact = document.createElement('li');
-  contact.id = 'contact';
+  contact.className = 'headerBtn';
   contact.textContent = 'Contact';
+
+  home.addEventListener('click', () => {
+    clearContent();
+    createHome();
+  });
+
+  menu.addEventListener('click', () => {
+    clearContent();
+    createMenu();
+  });
+
+  contact.addEventListener('click', () => {
+    clearContent();
+    console.log('Contact button!');
+  });
 
   header.appendChild(logo);
   header.appendChild(list);
